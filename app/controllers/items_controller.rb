62cc52afad2ab2a85dc03025afd859190c1d9d00
nemuo1208 <151502454+nemuo1208@users.noreply.github.com>
2023-12-21 13:27:20 +0900

    商品詳細表示機能第一回

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:new, :create, :edit, :update]
 
   def index
+    @items = Item.all
     @items = Item.order("created_at DESC")
   end
 
@@ -10,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -18,15 +19,33 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  #   @item = Item.find(params[:id])
+  def show
+  end
+
+  # def edit
   # end
 
-
+  # def update
+  #   if @item.update(item_params)
+  #     redirect_to root_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   private
   def item_params
     params.require(:item).permit(:name, :price, :explanation, :category_id, :product_condition_id, :delivery_charge_id, :area_of_origin_id, :days_to_ship_id, :image, ).merge(user_id: current_user.id)
   end
 
+  def ensure_current_user
+    item = Item.find(params[:id])
+    if item.user_id != current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
